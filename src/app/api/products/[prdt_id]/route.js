67b1583e32bcab7ae3814a1f10365a2ea209128a
Rigoberto Alcantar Aguilar <a20150338@utem.edu.mx>
2023-12-14 11:55:40 -0600

    finish with api products

--- src/app/api/products/[prdt_id]/route.js
+++ src/app/api/products/[prdt_id]/route.js
@@ -9,3 +9,28 @@ export async function GET(request, { params }) {
     return NextResponse.json({ message: 'Not found' }, { status: 500 })
   }
 }
+
+export async function DELETE(request, { params }) {
+  try {
+    const result = await pool.query('DELETE FROM products WHERE prdt_id = ?', params.prdt_id)
+
+    if (result.affectedRows === 0) { throw new Error('Product not found') }
+
+    return NextResponse.json({ message: 'Product deleted' }, { status: 204 })
+  } catch (error) {
+    return NextResponse.json({ message: 'Not found' }, { status: 404 })
+  }
+}
+
+export async function PUT(request, { params }) {
+  try {
+    const data = await request.json()
+    const result = await pool.query('UPDATE products SET ? where prdt_id = ?', [data, params.prdt_id])
+
+    if (result.affectedRows === 0) { throw new Error('Product not found') }
+
+    return NextResponse.json({ message: 'Product updated' }, { status: 201 })
+  } catch (error) {
+    return NextResponse.json({ message: 'Product not found' }, { status: 404 })
+  }
+}
