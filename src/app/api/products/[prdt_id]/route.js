695ace5a5a271fc5f0cc023012216ca2ec5f0f49
Rigoberto Alcantar Aguilar <a20150338@utem.edu.mx>
2023-12-13 18:50:12 -0600

    add get product by id

--- src/app/api/products/[prdt_id]/route.js
+++ src/app/api/products/[prdt_id]/route.js
@@ -1,6 +1,11 @@
 import { pool } from '@/libs/mysql'
 import { NextResponse } from 'next/server'
 
-export function GET() {
-  return NextResponse.json('mostrando un producto')
+export async function GET(request, { params }) {
+  try {
+    const result = await pool.query('SELECT * FROM products WHERE prdt_id = ?', params.prdt_id)
+    return NextResponse.json(result[0])
+  } catch (error) {
+    return NextResponse.json({ message: 'Not found' }, { status: 500 })
+  }
 }
